f3496877d2a78d78f1446e829488f9371204ca50
IBUNHABIB <ibunhabib@gmail.com>
2019-12-13 16:14:23 +0300

    correcting lint errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-    validates :content, presence: true, length: { minimum: 20 }
+  belongs_to :user
+  belongs_to :post
+  validates :content, presence: true, length: { minimum: 20 }
 end
