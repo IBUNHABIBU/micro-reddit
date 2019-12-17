6fed6b899089004f1fbe1c6853f49b409dacdd3f
IBUNHABIB <ibunhabib@gmail.com>
2019-12-17 14:41:48 +0300

    correcting the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 # frozen_string_literal: true`
-=======
-# frozen_string_literal: true
-
->>>>>>> 56c7d3a182ff64a3e599feb681f9e51cbb040dc8
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
   validates :name, presence: true
-<<<<<<< HEAD
   validates :email, presence: true
-=======
->>>>>>> 56c7d3a182ff64a3e599feb681f9e51cbb040dc8
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,7 +7,6 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-<<<<<<< HEAD
     @user = User.new(name: "Masanja", email: "kadhafi@yahoo.com")
   end
   test "should be valid" do
@@ -15,16 +14,11 @@ class UserTest < ActiveSupport::TestCase
   end
   test "name should be present" do
     @user.name = "     "
-=======
-    @user = User.new(name: 'Kadhafi', email: 'kadhafi@yahoo.com')
+    assert_not @user.valid?
   end
 
-  test 'should be valid' do
-    assert @user.valid?
-  end
-  test 'name should be present' do
-    @user.name = '     '
->>>>>>> 56c7d3a182ff64a3e599feb681f9e51cbb040dc8
+  test 'email should be present' do
+    @user.email = " "
     assert_not @user.valid?
   end
 end
