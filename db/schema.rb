1fa92e442a8994fecef9d75b2925160cbc4353a8
IBUNHABIB <ibunhabib@gmail.com>
2019-12-19 23:14:16 +0300

    making form submission work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_001413) do
+ActiveRecord::Schema.define(version: 2019_12_19_141023) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_12_12_001413) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
